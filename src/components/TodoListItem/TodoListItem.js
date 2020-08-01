8d7b1fedb86ad420386d989b4b8c4b1c3919075e
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-08-01 22:13:01 +0300

    refactor: add classes

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,32 +3,37 @@ import React from 'react';
 //style
 import './TodoListItem.scss';
 
-const TodoListItem = ({ label, important = false }) => {
+class TodoListItem extends React.Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+  render() {
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}>
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 export default TodoListItem;
